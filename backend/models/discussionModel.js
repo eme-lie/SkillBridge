435f0457865cd9bd5d71c23e872b309d41504e8e
Chukwuemelie <obioraemelie@gmail.com>
2024-07-31 14:24:59 +0100

    update the discussion model

--- backend/models/discussionModel.js
+++ backend/models/discussionModel.js
@@ -1,16 +1,36 @@
 import mongoose from "mongoose";
 
+const replySchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+    content: { type: String, required: true },
+    upvotes: { type: Number, default: 0 },
+    replies: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "User",
+        },
+        content: { type: String, required: true },
+        upvotes: { type: Number, default: 0 },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
 const discussionSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    sector: { type: String, required: true },
     tags: [{ type: String }],
+    replies: [replySchema],
+    upvotes: { type: Number, default: 0 },
   },
   { timestamps: true }
 );
 
-const Discussion = mongoose.model("Discussion", discussionSchema);
-
-export default Discussion;
+module.exports = mongoose.model("Discussion", discussionSchema);
